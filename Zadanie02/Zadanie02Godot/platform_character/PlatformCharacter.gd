446bc40b5ff14c168936179668dc0974df48e290
Jakub Okoń <60440859+JakePerch@users.noreply.github.com>
2021-01-13 14:38:31 +0100

    13-01

--- Zadanie02/Zadanie02Godot/platform_character/PlatformCharacter.gd
+++ Zadanie02/Zadanie02Godot/platform_character/PlatformCharacter.gd
@@ -8,16 +8,23 @@ export (float, 0, 1.0) var acceleration = 0.25
 
 var velocity = Vector2.ZERO
 
+func _ready():
+	$AnimatedSprite.playing = true
+
 func get_input():
 	var dir = 0
 	if Input.is_action_pressed("walk_right"):
 		dir += 1
+		$AnimatedSprite.flip_h = false
 	if Input.is_action_pressed("walk_left"):
 		dir -= 1
+		$AnimatedSprite.flip_h = true
 	if dir != 0:
 		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+		$AnimatedSprite.animation = "run"
 	else:
 		velocity.x = lerp(velocity.x, 0, friction)
+		$AnimatedSprite.animation = "idle"
 	
 func _physics_process(delta):
 	get_input()
